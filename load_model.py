d7fbbbd4319aff67beeba4713ec3293f3a4a3a3b
Antony-M1 <96291963+Antony-M1@users.noreply.github.com>
2024-09-17 11:08:40 +0530

    load the inpaint model also

--- load_model.py
+++ load_model.py
@@ -1,17 +1,23 @@
 import torch
 import time
 import os
-from diffusers import StableDiffusionUpscalePipeline
+from diffusers import StableDiffusionUpscalePipeline, AutoPipelineForInpainting
 from dotenv import load_dotenv
 
 
 load_dotenv()
-model_id = os.getenv("MODEL_NAME")
+inpainting_model_id = os.getenv("INPAINTING_MODEL_NAME", "stabilityai/stable-diffusion-2-inpainting")
+upscale_model_id = os.getenv("UPSCALE_MODEL_NAME", "stabilityai/stable-diffusion-x4-upscaler")
 try:
     start_time = time.time()
-    pipeline = StableDiffusionUpscalePipeline.from_pretrained(
-        model_id, revision="fp16", torch_dtype=torch.float16
-    )
+    upscale_pipeline = StableDiffusionUpscalePipeline.from_pretrained(
+        upscale_model_id, revision="fp16", torch_dtype=torch.float16
+    ).to("cuda")
+    
+    # Load the inpainting pipeline
+    inpainting_pipeline = AutoPipelineForInpainting.from_pretrained(
+        inpainting_model_id, revision="fp16", torch_dtype=torch.float16
+    ).to("cuda")
     end_time = time.time()
     print(f"Time taken to load pipeline: {end_time - start_time} seconds")
 except Exception as e:
